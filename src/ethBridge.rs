aaef64816d09758437f2c8d5689db1f3335db3f4
Yoann Thomas <tho994@gmail.com>
2021-05-31 18:03:41 +0200

    FIx all eth call errors

--- src/ethBridge.rs
+++ src/ethBridge.rs
@@ -6,22 +6,23 @@ use crate::backend_mecha_function::BackEndMechaFunction;
 use web3::futures::Future;
 use web3::contract::{Contract, Options};
 use web3::types::{TransactionRequest, Address, U256};
-use web3::transports::Http;
+use web3::transports::{Http, EventLoopHandle};
+
 
 
 pub struct ETHNFTContract {
     mecha_contract_nft: Contract<Http>,
     player_address: Address,
+    http_event_loop: EventLoopHandle
 }
 
 impl ETHNFTContract {
-    fn new(hexa_contract_address: String, hexa_player_address: String) -> Self {
-        let (_, http) = web3::transports::Http::new(
+    pub(crate) fn new(hexa_contract_address: String, hexa_player_address: String) -> Self {
+        let (eloop, http) = web3::transports::Http::new(
             "http://localhost:8545"
         ).unwrap();
 
         let web3 = web3::Web3::new(http);
-        let accounts = web3.eth().accounts().wait().unwrap();
 
         let contract_address: Address = hexa_contract_address.parse().unwrap();
 
@@ -29,7 +30,8 @@ impl ETHNFTContract {
 
         ETHNFTContract {
             mecha_contract_nft: contract,
-            player_address: hexa_contract_address.parse().unwrap()
+            player_address: hexa_player_address.parse().unwrap(),
+            http_event_loop: eloop
         }
     }
 }
@@ -50,7 +52,8 @@ impl BackEndMechaFunction for ETHNFTContract {
     fn generate_new_mecha(&mut self, mecha_name: String) {
 
         let mint_options = Options::with(|opt| opt.gas = Some(3_000_000.into()));
-        let tx_hash = self.mecha_contract_nft.call("mint", (), self.player_address, mint_options).wait().unwrap();
+
+        let tx_hash = self.mecha_contract_nft.call("mint", (mecha_name), self.player_address, mint_options).wait().unwrap();
     }
 
     fn get_total_mecha_owned(&mut self) -> U256 {
@@ -81,19 +84,48 @@ impl BackEndMechaFunction for ETHNFTContract {
     }
 }
 
-fn GetMecha() {
-
-
-
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::ethBridge::GetMecha;
+    use crate::ethBridge::{ETHNFTContract};
+    use crate::backend_mecha_function::BackEndMechaFunction;
+    use web3::types::{U256};
+    use super::web3::futures::Future;
+
+    const PLAYER_ADDRESS: &str = "F2b2208cecb42a55Fd328E871B2d04C85e91Bd5E";
+    const MECHA_CONTRACT_ADDRESS: &str = "1E63952E734616475A53f4c0D62D78969549D215";
+
+    fn setup() -> ETHNFTContract {
+        ETHNFTContract::new(
+            MECHA_CONTRACT_ADDRESS.to_string(),
+            PLAYER_ADDRESS.to_string()
+        )
+    }
 
     #[test]
-    fn test_call() {
-        GetMecha();
+    fn test_generate() {
+        let mut mecha_NFT_contract = setup();
+
+        mecha_NFT_contract.generate_new_mecha("toto".to_string());
     }
+
+    #[test]
+    fn test_get_balance_of() {
+
+        let mut mecha_NFT_contract = setup();
+
+        assert_eq!(mecha_NFT_contract.get_total_mecha_owned(), U256::from(0));
+
+    }
+    #[test]
+    fn test_connexion() {
+        let (_eloop, http) = web3::transports::Http::new(
+            "http://localhost:8545"
+        ).unwrap();
+
+        let web3 = web3::Web3::new(http);
+        let accounts = web3.eth().accounts().wait().unwrap();
+        println!("Accounts: {:?}", accounts);
+    }
+
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use crate::mecha::{Mecha, MechaColor};
 use crate::mecha_collection::CachedMechaCollection;
+use crate::ethBridge::ETHNFTContract;
+use std::io::{stdin, Error};
+use std::str::FromStr;
+use crate::backend_mecha_function::BackEndMechaFunction;
+use std::io;
 
 mod mecha;
 mod mecha_state;
@@ -8,53 +13,92 @@ mod mecha_collection;
 mod backend_mecha_function;
 mod mock_backend_function;
 
+const PLAYER_ADDRESS: &str = "F2b2208cecb42a55Fd328E871B2d04C85e91Bd5E";
+const MECHA_CONTRACT_ADDRESS: &str = "1E63952E734616475A53f4c0D62D78969549D215";
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut nbTurn = 1;
 
-    let mut battling_mecha = [
-        Mecha::new(String::from("YTH"), 25, 1, 1, MechaColor::Red),
-        Mecha::new(String::from("CPU"), 2, 2, 2, MechaColor::Blue),
-    ];
+    let mut mecha_NFT_contract = ETHNFTContract::new(
+        MECHA_CONTRACT_ADDRESS.to_string(),
+        PLAYER_ADDRESS.to_string()
+    );
 
-    //let mut user_mecha_list: Option<CachedMechaCollection> = CachedMechaCollection::new ;
-
-
-
+    let mut nb_mecha;
     loop {
-        print!("Turn {} ", nbTurn);
-
-        if let Some((First, Elements)) = battling_mecha.split_first_mut() {
-
-            if let Some(Second) = Elements.get_mut(0) {
-
-                let fastest;
-                let slowest;
-                if First.speed >= Second.speed {
-                    fastest = First;
-                    slowest = Second;
-                } else {
-                    fastest = Second;
-                    slowest = First;
-                }
-
-                println!("{} attack", fastest.name);
-                fastest.AttackOpponent(slowest);
-                if CheckKO(slowest){
-                    break;
-                }
-
-                println!("{} attack", slowest.name);
-                slowest.AttackOpponent(fastest);
-                if CheckKO(fastest){
-                    break;
-                }
+        let mut buffer = String::new();
+        println!("How many mecha create ?");
+        stdin().read_line(&mut buffer)?;
+        match u8::from_str(&buffer.trim_end()) {
+            Ok(nb) => {
+                nb_mecha = nb;
+                break;
             }
+            Err(_) => { println!("NAN") }
         }
-        print_info(&battling_mecha);
-
-        nbTurn += 1;
     }
+
+
+    for i in 0..nb_mecha {
+        let mut buffer = String::new();
+        println!("mecha name ?");
+        stdin().read_line(&mut buffer)?;
+        buffer.trim_end();
+        mecha_NFT_contract.generate_new_mecha(buffer);
+    }
+    Ok(())
+
+    // let mut user_mecha_list= CachedMechaCollection::new(mecha_NFT_contract);
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // let mut battling_mecha = [
+    //     Mecha::new(String::from("YTH"), 25, 1, 1, MechaColor::Red),
+    //     Mecha::new(String::from("CPU"), 2, 2, 2, MechaColor::Blue),
+    // ];
+    //
+    // //let mut user_mecha_list: Option<CachedMechaCollection> = CachedMechaCollection::new ;
+    //
+    //
+    //
+    // loop {
+    //     print!("Turn {} ", nbTurn);
+    //
+    //     if let Some((First, Elements)) = battling_mecha.split_first_mut() {
+    //
+    //         if let Some(Second) = Elements.get_mut(0) {
+    //
+    //             let fastest;
+    //             let slowest;
+    //             if First.speed >= Second.speed {
+    //                 fastest = First;
+    //                 slowest = Second;
+    //             } else {
+    //                 fastest = Second;
+    //                 slowest = First;
+    //             }
+    //
+    //             println!("{} attack", fastest.name);
+    //             fastest.AttackOpponent(slowest);
+    //             if CheckKO(slowest){
+    //                 break;
+    //             }
+    //
+    //             println!("{} attack", slowest.name);
+    //             slowest.AttackOpponent(fastest);
+    //             if CheckKO(fastest){
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     print_info(&battling_mecha);
+    //
+    //     nbTurn += 1;
+    // }
 }
 
 fn CheckKO(mecha: &Mecha) -> bool {
